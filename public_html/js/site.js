28fc675f0a462912652e1679a3ffff07691a7108
dunerunner <powercrush11@gmail.com>
2014-11-13 13:09:38 +0300

    Added Event Delegation

--- public_html/js/site.js
+++ public_html/js/site.js
@@ -15,19 +15,14 @@ $(document).ready(function () {
         }
         my_interests.push(name);
         var my_list = $('#user_interests');
-        generateInterest(my_list, name, my_interests.length - 1, true);
+        generateInterest(my_list, name, true);
         $('#add_interest').val('');
         $('#info_add_alert').text('');
     }
-    function removeInterest(name) {
-        my_interests.forEach(function (element, index) {
-            if (element === name) {
-                my_interests.splice(index, 1);
-                var to_remove = $('#user_interests li')[index];
-                to_remove.remove();
-                return;
-            }
-        });
+    function removeInterest(index) {
+        my_interests.splice(index, 1);
+        var to_remove = $('#user_interests li')[index];
+        to_remove.remove();
     }
     function addFriendInterest(name, element) {
         for (var i = 0, len = my_interests.length; i < len; i++) {
@@ -38,77 +33,51 @@ $(document).ready(function () {
         }
         my_interests.push(name);
         var my_list = $('#user_interests');
-        generateInterest(my_list, name, my_interests.length - 1, true);
+        generateInterest(my_list, name, true);
 
-        element.unbind('mouseenter mouseleave');
-
-        element.find('.b-interest__name').toggleClass('active');
-        element.find('.b-interest__control').toggle();
-        element.find('.b-interest__added').toggleClass('b-interest__added_hidden');
-        element.find('.b-interest__complain').toggleClass('b-interest__complain_hidden');
+        $(element).addClass('added')
+                .find('.b-interest__name').toggleClass('active').end()
+                .find('.b-interest__control').toggle().end()
+                .find('.b-interest__added').toggleClass('b-interest__added_hidden').end()
+                .find('.b-interest__complain').toggleClass('b-interest__complain_hidden').end();
     }
-    function generateInterest(list, name, index, isMine) {
+    function generateInterest(list, name, isMine) {
         var li = $('<li class="b-interest-list__element"/>').appendTo(list);
         var interest = $('<div class="b-interest"/>')
                 .appendTo(li);
-        var action_control = $('<a class="b-interest__control"/>')
+        $('<a class="b-interest__control"/>')
                 .attr('href', '')
                 .appendTo(interest);
         if (!isMine) {
-            var complaint_control = $('<a class="b-interest__complain b-interest__complain_hidden" />')
+            $('<a class="b-interest__complain b-interest__complain_hidden" />')
                     .attr('href', '')
                     .text('пожаловаться')
                     .appendTo(interest);
         }
         var interest_name = $('<div class="b-interest__name"/>')
                 .appendTo(interest);
-        var interest_text = $('<div class="b-interest__text"/>')
+        $('<div class="b-interest__text"/>')
                 .text(name)
                 .appendTo(interest_name);
-        var text_fader = $('<div class="b-interest__fader"/>')
+        $('<div class="b-interest__fader"/>')
                 .addClass('info_fader')
                 .appendTo(interest_name);
-        if (isMine) {
-            action_control.on('click', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                removeInterest(name);
-            });
-            li.on('mouseenter mouseleave', function () {
-                action_control.toggleClass('b-interest__control_del');
-            });
-        } else {
-            var added_info = $('<div class="b-interest__added b-interest__added_hidden"/>')
+        if (!isMine) {
+            $('<div class="b-interest__added b-interest__added_hidden"/>')
                     .text('добавлено в ваши увлечения')
                     .appendTo(interest_name);
-            action_control.on('click', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                addFriendInterest(name, li);
-            });
-            complaint_control.on('click', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                $(".b-popup-cover").fadeIn(500);
-                $('.b-popup__interest-name').text('"' + friend_interests[index] + '"');
-            });
-            li.on('mouseenter mouseleave', function () {
-                action_control.toggleClass('b-interest__control_add');
-                complaint_control.toggleClass('b-interest__complain_hidden');
-                interest_name.toggleClass('active');
-            });
         }
     }
     function generateMyInterestList() {
         var my_list = $('#user_interests');
-        my_interests.forEach(function (element, index) {
-            generateInterest(my_list, element, index, true);
+        my_interests.forEach(function (element) {
+            generateInterest(my_list, element, true);
         });
     }
     function generateFriendInterestList() {
         var my_list = $('#friend_interests');
-        friend_interests.forEach(function (element, index) {
-            generateInterest(my_list, element, index, false);
+        friend_interests.forEach(function (element) {
+            generateInterest(my_list, element, false);
         });
     }
 
@@ -124,5 +93,37 @@ $(document).ready(function () {
 
     $(".popup_close").on('click', function () {
         $(".b-popup-cover").fadeOut(500);
+    });
+    $('#user_interests').on('mouseenter mouseleave', '.b-interest-list__element', function () {
+        $(this).find('.b-interest__control').toggleClass('b-interest__control_del');
+    });
+    $('#user_interests').on('click', '.b-interest-list__element', function (e) {
+        if (e.target.nodeName === 'A') {
+            var elementIndex = $(this).index();
+            e.preventDefault();
+            e.stopPropagation();
+            removeInterest(elementIndex);
+        }
+    });
+    $('#friend_interests').on('mouseenter mouseleave', '.b-interest-list__element', function () {
+        if (!$(this).hasClass('added')) {
+            $(this).find('.b-interest__control').toggleClass('b-interest__control_add').end()
+                    .find('.b-interest__complain').toggleClass('b-interest__complain_hidden').end()
+                    .find('.b-interest__name').toggleClass('active');
+        }
+    });
+    $('#friend_interests').on('click', '.b-interest-list__element', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (e.target.nodeName === 'A') {
+            var interestName = $(this).find('.b-interest__text').text();
+            if ($(e.target).hasClass('b-interest__control')) {
+                addFriendInterest(interestName, this);
+                return;
+            }
+            $(".b-popup-cover").fadeIn(500);
+            $('.b-popup__interest-name').text('"' + interestName + '"');
+        }
+
     });
 });
